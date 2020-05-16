facecfaa4e54e3ed88cb6b3cf2668707268b4536
flashlan <flash_lan@hotmail.com>
2020-05-16 01:48:15 -0300

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -8,12 +8,13 @@ Created on Fri May 15 23:53:28 2020
 def  bubble_sort(lista):
     fim = len(lista)
     for i in range(fim -1, 0 ,-1):
-        trocou = False
+       
         for j in range(i):
             if lista[j] > lista[j+1]:
                 lista[j], lista[j+1] = lista[j+1], lista[j]
-                trocou = True
+                
                 print(lista)
-        if trocou == False:
-            return lista     
+     
+    return lista     
+     
     
